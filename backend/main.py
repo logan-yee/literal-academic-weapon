3e75fa40c124ecfae681ba85973aeebb066513e2
logan-yee <logany33@gmail.com>
2025-02-08 01:14:20 -0500

    main

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from analyze_screenshots import capture_and_analyze_screenshots
+from stat_parser import run_schedule_analysis
+from schedule_creator import run_schedule_creator
+
+def main():
+    # run_schedule_analysis()
+    run_schedule_creator()
+    # capture_and_analyze_screenshots()
+
+
+if __name__ == "__main__":
+    main()
